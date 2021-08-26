ccfdc72e1ea13c236ee13f6f3aa2d4d31a069f4d
sungalex <cloudallera@gmail.com>
2021-08-27 00:06:32 +0900

    More Routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,9 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller()
+@Controller('movies')
 export class MoviesController {
   @Get()
   getAll() {
     return 'This will return all movies';
+  }
+
+  @Get('search')
+  search(@Query('year') seachingYear: string) {
+    return `We are searching for a movie made after: ${seachingYear}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string) {
+    return `This will return one movie with the id: ${movieId}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return movieData;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
